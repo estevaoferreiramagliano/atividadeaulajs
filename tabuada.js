d7a00fbc7fe69354acead66eb27e61e33ca806ab
Estevao Magliano <ferreiramagliano.info@gmail.com>
2026-07-01 20:01:59 -0300

    tabuada

---
+++ tabuada.js
@@ -0,0 +1,7 @@
+let numero = 1;
+for (let i = 1; i <=9; i++){
+    console.log (numero + "x" + i + " = " + (numero *i))
+    for (let j = 1; j <= 9; j++){
+        console.log (j + "x" + numero + " = " + (numero *j))
+    }
+}
