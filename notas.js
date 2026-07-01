15501c119a12bc7fb8fedad884be0d0f32f03e5b
Estevao Magliano <ferreiramagliano.info@gmail.com>
2026-06-30 21:26:43 -0300

    sistema de notas

---
+++ notas.js
@@ -0,0 +1,9 @@
+let nota = 50;
+
+if (nota >= 75) {
+    console.log("Aluno aprovado.");
+} else if (nota >= 60 && nota <= 74) {
+    console.log("Aluno em recuperação.");
+} else {
+    console.log("Aluno reprovado.");
+}
