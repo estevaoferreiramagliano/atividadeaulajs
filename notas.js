305dc25a7d7feec38decbd38a026326cf81f6896
Estevao Magliano <ferreiramagliano.info@gmail.com>
2026-07-01 18:12:52 -0300

    descrição do código notas

--- notas.js
+++ notas.js
@@ -1,8 +1,10 @@
 let nota = 50;
 
 if (nota >= 75) {
+    //Em português: Se a nota for maior ou igual a 75...
     console.log("Aluno aprovado.");
 } else if (nota >= 60 && nota <= 74) {
+    //Em português:Senão, se a nota estiver entre 60 e 74...
     console.log("Aluno em recuperação.");
 } else {
     console.log("Aluno reprovado.");
