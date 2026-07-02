f7d1088fd0d95be032e109c8cf4b7da645ccd1f6
Estevao Magliano <ferreiramagliano.info@gmail.com>
2026-07-02 20:33:30 -0300

    csalculadora

---
+++ calculadora.js
@@ -0,0 +1,37 @@
+function soma(a, b){
+    if (typeof(a)!== "number" || typeof(b)!==  "number"){
+        throw new Error("Os dois valores devem ser números.");
+    }
+    return a + b
+}
+function subtracao(a, b){
+    if (typeof(a)!== "number" || typeof(b)!==  "number"){
+        throw new Error("Os dois valores devem ser números.");
+    }
+    return a - b
+}
+function multiplica(a, b){
+    if (typeof(a)!== "number" || typeof(b)!==  "number"){
+        throw new Error("Os dois valores devem ser números.");
+    }
+    return a * b
+}
+function divide(a, b){
+    if (typeof(a)!== "number" || typeof(b)!==  "number"){
+        throw new Error("Os dois valores devem ser números.");
+    }
+    if (b===0){
+        throw new Error("Não é possível dividir por zero")
+    }
+    
+    return a / b
+}
+
+try{
+    console.log("soma:",soma(5, 6));
+    console.log("subtração:",subtracao(20, 6));
+    console.log("multiplicação:",multiplica(9, 6));
+    console.log("divisão:",divide(126, 6));
+}catch (erro) {
+    console.log("Erro:", erro.message);
+}
