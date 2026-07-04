216b27433502ea3dd292818d88fdda00dd63cafe
Estevao Magliano <ferreiramagliano.info@gmail.com>
2026-07-03 21:46:34 -0300

    validar correto

--- Calculadora/validar.js
+++ Calculadora/validar.js
@@ -2,16 +2,4 @@ export function validar( a, b){
     if (typeof(a)!== "number" || typeof(b)!==  "number"){
         throw new Error("Os dois valores devem ser números.");
     }
-    if (typeof(a)!== "number" || typeof(b)!==  "number"){
-        throw new Error("Os dois valores devem ser números.");
-    }
-    if (typeof(a)!== "number" || typeof(b)!==  "number"){
-        throw new Error("Os dois valores devem ser números.");
-    }
-    if (b===0){
-        throw new Error("Não é possível dividir por zero")
-    }
-    if (typeof(a)!== "number" || typeof(b)!==  "number"){
-        throw new Error("Os dois valores devem ser números.");
-    }
 }
